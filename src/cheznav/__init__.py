f807d470be19965712ca96507c9bd1ee8f17c2a3
David Jetelina <david@djetelina.cz>
2026-04-02 23:16:58 +0200

    chore: simplify version resolution in __init__.py

--- src/cheznav/__init__.py
+++ src/cheznav/__init__.py
@@ -1,8 +1,5 @@
-from importlib.metadata import PackageNotFoundError, version
+from importlib.metadata import version
 
-try:
-    __version__ = version("cheznav")
-except PackageNotFoundError:
-    __version__ = "0.1.0"
+__version__ = version("cheznav")
 
 THEME = "dracula"
